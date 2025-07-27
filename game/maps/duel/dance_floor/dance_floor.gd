18efbcced7f08c6790b165650a517a7d72a20706
Summersay415 <summersay415@gmail.com>
2025-07-27 13:04:28 +0700

    Переработка карты Танцпол!

--- game/maps/duel/dance_floor/dance_floor.gd
+++ game/maps/duel/dance_floor/dance_floor.gd
@@ -1,14 +1,26 @@
 extends Map
 
-@onready var _anim: AnimationPlayer = $TileMapLayers/AnimationPlayer
+@export var background_color: Color
+var _previous_color: Color
+
+@onready var _anim: AnimationPlayer = $TileMapLayers/WallsColored/AnimationPlayer
 
 func _initialize() -> void:
+	_previous_color = RenderingServer.get_default_clear_color()
+	if Globals.get_setting_bool("low_graphics"):
+		RenderingServer.set_default_clear_color(background_color)
+	
 	if event:
 		var duel: Duel = event
 		duel.round_started.connect(_on_round_started)
 		duel.round_ended.connect(_on_round_ended)
 	else:
 		_on_round_started()
+
+
+func _exit_tree() -> void:
+	if RenderingServer.get_default_clear_color() != _previous_color:
+		RenderingServer.set_default_clear_color(_previous_color)
 
 
 func _on_round_started() -> void:
