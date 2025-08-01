806643300d76b65b73076e51147508e6809d78a9
Summersay415 <summersay415@gmail.com>
2025-08-01 21:32:48 +0700

    По мелочи

--- game/skins/videogames/dealer/dealer.gd
+++ game/skins/videogames/dealer/dealer.gd
@@ -1,5 +1,6 @@
 extends PlayerSkin
 
+@export var normal_texture: Texture2D
 @export var damaged_texture: Texture2D
 
 func _initialize() -> void:
@@ -9,3 +10,5 @@ func _initialize() -> void:
 func _on_player_health_changed(old_value: int, new_value: int) -> void:
 	if old_value > new_value:
 		texture = damaged_texture
+	elif new_value > old_value:
+		texture = normal_texture
